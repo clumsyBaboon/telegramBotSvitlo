d3ce6a99c0422263f8265776235db32ed6ffa925
clumsyBaboon <godnicnic@gmail.com>
2026-01-30 13:22:44 +0200

    auto 30.01.2026  13:22:44,46

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("hello  World!!!!!");
+const { Telegraf } = require("telegraf");
+
+const TOKEN = process.env.BOT_TOKEN;
+const WEBHOOK_URL = process.env.RENDER_EXTERNAL_URL;
+const PORT = process.env.PORT || 3000;
+
+const bot = new Telegraf(TOKEN);
+
+bot.start((ctx) => ctx.reply("Bot started!"));
+
+bot.launch({
+    webhook: {
+        domain: WEBHOOK_URL,
+        port: PORT
+    }
+})
+.then(() => console.log("Server has started!"))
+.catch((err) => console.error(`Err ${err}`));
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
