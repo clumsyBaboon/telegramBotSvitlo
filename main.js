b6e85a9fe4b3c2841439461c085f7f6aef3e5c99
clumsyBaboon <godnicnic@gmail.com>
2026-01-30 15:19:49 +0200

    auto 30.01.2026  15:19:49,28

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { Telegraf } = require("telegraf");
 const admin = require("firebase-admin");
+const ping = require("ping");
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY)
 
@@ -26,9 +27,9 @@ bot.command("set", async (ctx) => {
             data: text
         }, { merge: true })
 
-        ctx.reply("Данные сохранены!")
+        ctx.reply("Host збережено!")
     } catch (err) {
-        ctx.reply("Ошибка сохранения")
+        ctx.reply("Firebase не доступній")
     }
 })
 
@@ -38,13 +39,33 @@ bot.command("get", async (ctx) => {
     try {
         const doc = await userRef.doc(userID).get();
 
-        if (!doc.exists) return ctx.reply("У вас еще нет сохраненных данных");
+        if (!doc.exists) return ctx.reply("У вас ще немає звереженного host, /set для збереження");
 
-        const data = doc.data();
-        ctx.reply(`Ваши данные ${data.data}`);
+        const data = doc.data().data;
+        ctx.reply(`Ваш host ${data}`);
     } catch (err) {
-        ctx.reply("Не удалось получить данные!");
+        ctx.reply("Firebase не доступній");
     }
+})
+
+bot.command("ping", async (ctx) => {
+    const userID = String(ctx.frome.id);
+    let data;
+    ctx.reply(`Перевіряю ${data}, timeout = 10s`);
+    try {
+        const doc = await userRef.doc(userID).get();
+        if (!doc.exists) return ctx.reply("У вас ще немає звереженного host, /set для збереження");
+        data = doc.data().data;
+    } catch (err) {
+        ctx.reply("Firebase не доступній")
+    }
+    try {
+        const res = await ping.promise.probe(data, { timeout: 10 });
+        ctx.reply(res.alive ? `Сервер онлайн. (відгук: ${res.time}мс)` : `Сервер наразі офлайн (timeout = 10s)`);
+    } catch (err) {
+        ctx.reply("Помилка, під час виконання ping");
+    }
+    
 })
 
 bot.launch({
