87f75f72b2f805ea58b502b142b7d854e424fe81
clumsyBaboon <godnicnic@gmail.com>
2026-02-17 16:31:35 +0200

    auto 17.02.2026  16:31:35,13

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 const { Telegraf } = require("telegraf");
+const admin = require("firebase-admin");
 const net = require("net");
 const express = require("express");
 const http = require("http");
 
 const app = express();
 
-const devices = new Map();
-
 app.get("/update", (req, res) => res.send("Bot active!"));
-app.get("/ping", (req, res) => {
+app.get("/ping", async (req, res) => {
     res.send("ok");
-    console.log(req.query.device_id);
-    devices.set("deviceID", req.query.device_id);
+    const data = req.query.device_id;
+    if (!data || data == null) return
+    try {
+        await userRef.doc(device_id).set({
+            lastUpdated: Date.now()
+        }, { merge: true })
+    } catch (err) {
+        console.error("Error update firestore");
+    }
 })
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
+const db = admin.firestore();
+const userRef = db.collection("devices");
 
 const TOKEN = process.env.BOT_TOKEN;
 const WEBHOOK_URL = process.env.RENDER_EXTERNAL_URL;
